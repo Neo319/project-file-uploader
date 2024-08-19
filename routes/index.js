528b80f255f404846948d1c23ac36ddce4ba0148
CastorDisaster <alexmnevins@gmail.com>
2024-08-19 10:00:37 -0400

    create base authentication views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Homepage" });
+});
+
+router.get("/sign-up", function (req, res) {
+  res.render("sign-up-form", { title: "Sign Up" });
 });
 
 module.exports = router;
