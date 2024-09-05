6d746964878e2ceb21bf66b0e39ac6222d5551d8
CastorDisaster <alexmnevins@gmail.com>
2024-09-05 11:16:01 -0400

    write database query in passport config

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(
 //initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- config/passport.js
+++ config/passport.js
@@ -2,9 +2,29 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 //require db through prisma?
 
-app.use("/", indexRouter);
+const prisma = require("@prisma/client").PrismaClient;
+const db = new prisma();
 
 module.exports = function (passport) {
   // ...
   // TODO : configure passport using prisma framework
+
+  const strategy = new LocalStrategy(function verify(user, password, done) {
+    // 1: database query
+    db.user
+      .findUnique({
+        where: { username: username },
+      })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+      });
+
+    // 2: encrypt with bcrypt
+
+    // 3: implement serialize and deserialize functions
+  });
 };
