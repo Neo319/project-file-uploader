d53c7f8dd06b301b365c4ec50bee00ada975dabb
CastorDisaster <alexmnevins@gmail.com>
2024-09-05 11:37:22 -0400

    add bcrypt compare function to passport configuration

--- config/passport.js
+++ config/passport.js
@@ -5,26 +5,43 @@ const LocalStrategy = require("passport-local");
 const prisma = require("@prisma/client").PrismaClient;
 const db = new prisma();
 
+const bcrypt = require("bcryptjs");
+
 module.exports = function (passport) {
   // ...
   // TODO : configure passport using prisma framework
 
-  const strategy = new LocalStrategy(function verify(user, password, done) {
+  const strategy = new LocalStrategy(async function verify(
+    user,
+    password,
+    done
+  ) {
     // 1: database query
-    db.user
-      .findUnique({
+    try {
+      // Use Prisma to find the user by username
+      const user = await prisma.user.findUnique({
         where: { username: username },
-      })
-      .then((user) => {
-        if (!user) {
-          return done(null, false, {
-            message: "Incorrect username or password.",
-          });
-        }
       });
 
-    // 2: encrypt with bcrypt
+      if (!user) {
+        return done(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
 
-    // 3: implement serialize and deserialize functions
+      // 2: verify encrypted password with bcrypt compare
+      const match = await bcrypt.compare(password, user.hashedPassword);
+      if (!match) {
+        return done(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
+
+      // 3: implement serialize and deserialize functions
+
+      //
+    } catch (err) {
+      return done(err);
+    }
   });
 };
