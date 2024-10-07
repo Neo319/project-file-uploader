3209ea3c4cd4f62380675dc8f7d6df678ab9cf77
CastorDisaster <alexmnevins@gmail.com>
2024-10-07 12:29:53 -0400

    successfully log users in and out

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get("/", usersController.homepage_get);
 //TODO: change this to integrate passport authentication.
 router.post("/log-in", usersController.login_post);
 
-router.get("log-out", usersController.logout_get);
+router.get("/log-out", usersController.logout_get);
 
 router.get("/sign-up", usersController.sign_up_get);
 router.post("/sign-up", usersController.sign_up_post);
